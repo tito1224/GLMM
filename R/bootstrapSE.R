0d9732a065aaf48c53800cafa999de4cbd1c8eeb
tito1224 <64660812+tito1224@users.noreply.github.com>
2022-12-11 19:30:02 -0500

    Split functions into different files

---
+++ R/bootstrapSE.R
@@ -0,0 +1,22 @@
+bootstrapSE = function(n,df,dfname){
+  lstSE = numeric(0)
+
+  for(item in 1:n){
+    # simulate zi,mu_i,y_ij and add simulated seizures to dataset
+    dfSimulate = copy(epilepsy)
+    dfSimulate$seizures = simulateRandomEffect(df,dfname)$seizuresSimulate
+
+    # fit the model
+    model = run_model(dfSimulate, dfname)
+
+    # estimated beta parameters
+    estBetaSimulate = model$beta
+    B_expind_treat = as.numeric(unname(estBetaSimulate[4]))
+    lstSE = c(lstSE,B_expind_treat)
+  }
+
+  # calculate standard error of B_expind_treat
+  estSE = sd(lstSE)
+
+  return(estSE)
+}
