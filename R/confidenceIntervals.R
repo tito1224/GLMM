7994d0f75ae30b4db89308dc3d7e805965d15c59
azheng26 <azheng26@uwo.ca>
2022-12-13 05:42:32 -0500

    Implement confidence interval function to compute confidence intervals for all betas and sigmasq.

---
+++ R/confidenceIntervals.R
@@ -0,0 +1,15 @@
+ci <- function(data, example="epilepsy", B) {
+  estimates <- run_model(data, example)
+  n <- length(estimates$re)
+  btsp <- btsp(data, example, B)
+  interceptCI <- c(estimates$beta[1] - 1.96*btsp$interceptSE, estimates$beta[1] + 1.96*btsp$interceptSE)
+  ageCI <- c(estimates$beta[2] - 1.96*btsp$ageSE, estimates$beta[2] + 1.96*btsp$ageSE)
+  expindCI <- c(estimates$beta[3] - 1.96*btsp$expindSE, estimates$beta[3] + 1.96*btsp$expindSE)
+  expindtreatCI <- c(estimates$beta[4] - 1.96*btsp$expindtreatSE, estimates$beta[4] + 1.96*btsp$expindtreatSE)
+  sigmasqCI <- c((n-1)*estimates$sigmasq/qchisq(.025, df=n-1), (n-1)*estimates$sigmasq/qchisq(.025, df=n-1, lower.tail=F))
+  list(interceptCI = interceptCI,
+       ageCI = ageCI,
+       expindCI = expindCI,
+       expindtreatCI = expindtreatCI,
+       sigmasqCI = sigmasqCI)
+}
